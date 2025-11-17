d59b34feced8dfe0f305e3635fc64f1bde380485
Naveen <dnaveenvarma06@gmail.com>
2025-11-17 23:31:29 +0530

    fixed port issue

--- app.ts
+++ app.ts
@@ -39,7 +39,7 @@ app.post('/get-response', (req, res) => {
 });
 
 const domain = process.env.DOMAIN || "localhost"
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
+const PORT : number = (process.env.PORT || 4000) as number;
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on http://${domain}:${PORT}`);
 });
